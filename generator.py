070a5d4f286838dc4e0b3f34ccee33f475dbc8fa
Eduardo <>
2021-11-22 11:08:06 -0500

    fixing up for condition 2, working on 1

--- generator.py
+++ generator.py
@@ -10,7 +10,7 @@ import math
 
 
 #Create a random graph
-G = nx.gnp_random_graph(10, .1, directed=True)
+G = nx.gnp_random_graph(15, .3, directed=True)
 
 # Fix graph for condition 1: all zero in degree nodes come before others
 zeroes = sorted(list(G.in_degree(G.nodes())))
@@ -23,6 +23,8 @@ for node, indeg in zeroes:
         nonzero.append(node)
 
 num = len(zerol)
+highest_zero_node = num - 1
+# print("num " + str(num-1))
 d = dict()
 count = 0
 for i in zerol:
@@ -66,28 +68,89 @@ l = [list(islice(Inputt, elem))
 
 #Create a new filtered list that will shift items based on
 #if they violate condition 2 
-filtered_list = [[] for i in range(len(l))]
+filtered_list = [set() for i in range(len(l))]
 #by default the first partition won't violate anything bc no prev partition
 for tuple in l[0]: 
-    filtered_list[0].append(tuple)
+    filtered_list[0].add(tuple)
 
+# print(l)
 #SOMETHING IS WRONG HERE
 for i in range(len(l)-1):
     #get the highest node for that partition
     max_out_node = l[i][-1][1]
+    # print(max_out_node)
     #check the subsequent partitions
     for j in range(i+1, len(l)): 
+        # print("j: " + str(j))
         for tuple in l[j]:
             #if the node matches, pull in down to the current parition
             if tuple[1] == max_out_node:
-                filtered_list[i].append(tuple)
+                filtered_list[i].add(tuple)
             #otherwise add it to where it to next partition
             else: 
-                filtered_list[i+1].append(tuple) #actually should this be j idk ?
+                filtered_list[j].add(tuple) #actually should this be j idk ? was i+1 before
+    # print(filtered_list)
+    # print('\n')
+
+#original partitions 
+# for item in l:
+#     print(item)
+
+# print('\n')
+#if you comment this out it doesnt work 
+#this basically fixes the mistake im making above
+for i in range(len(filtered_list) - 1):
+    for tuple in filtered_list[i]:
+        if tuple in filtered_list[i+1]:
+            filtered_list[i+1].remove(tuple)
+        
+
+#fixed partitions
+# for item in filtered_list:
+#     print(item)
+#EXAMPLE OF CASE THAT FAILS CONDITION 2
+#original partitions
+# [(5, 2), (7, 3)]
+# [(1, 3), (3, 4)]
+# [(6, 4), (8, 5)]
+# [(3, 6), (2, 7), (3, 8), (0, 8), (8, 9)]
+
+#new partitions
+# {(1, 3), (7, 3), (5, 2)}
+# {(6, 4), (3, 4)}
+# {(6, 4), (8, 5)}
+# {(3, 8), (2, 7), (8, 9), (3, 6), (0, 8)}
+
+
+#ANOTHER CASE THAT ISNT TOTALLY CORRECT BUT WORKS?
+# [(0, 1), (2, 1), (6, 1)]
+# [(1, 2), (6, 3), (0, 4)]
+# [(5, 4), (8, 4), (4, 5)]
+# [(2, 6), (3, 7), (9, 8), (1, 9), (0, 9)]
+
+
+# {(0, 1), (6, 1), (2, 1)}
+# {(1, 2), (8, 4), (0, 4), (5, 4), (6, 3)}
+# {(4, 5), (5, 4), (8, 4)}                  the ones ending in 4 shouldnt be in this partition bc they were moved up 
+# {(3, 7), (0, 9), (2, 6), (9, 8), (1, 9)}
+
+
+#ANOTHER CASE IDK 
+# [(0, 4), (1, 4), (6, 5)]
+# [(3, 5), (7, 5), (4, 6)]
+# [(5, 6), (2, 6), (9, 6)]
+# [(8, 7), (4, 8), (9, 8), (4, 9)]
+
+
+# {(0, 4), (6, 5), (1, 4), (7, 5), (3, 5)}
+# {(9, 6), (4, 6), (2, 6), (5, 6)}
+# {(9, 6), (2, 6), (5, 6)}                      this should be empty i guess
+# {(8, 7), (4, 9), (9, 8), (4, 8)}
+
 
 #Assign labels to edges based on partitions we defined
-print(l)
-print(filtered_list)
+# print(l)
+# print(filtered_list)
 l = filtered_list
 labels = {}
 count = 0
@@ -126,6 +189,31 @@ for k,v in d.items():
         if int(ind) > int(max): 
             max = ind
 
+
+#FIXING NEW CASES OF ZERO IN DEGREE
+zeroes = sorted(list(G.in_degree(G.nodes())))
+zerol = []
+# nonzero = []
+for node, indeg in zeroes:
+    if indeg == 0:
+        zerol.append(int(node))
+    # else:
+    #     nonzero.append(node)
+
+# print(zerol)
+
+# filtered = filter(lambda score: score >= 70, scores)
+to_delete = list(filter(lambda high: high > highest_zero_node, zerol))
+
+print(to_delete)
+
+
+# G.remove_nodes_from(list(to_delete)) this should work but idk why it doesnt 
+
+for node in to_delete:
+    # print(str(node))
+    G.remove_node(str(node))
+
 nx.drawing.nx_pydot.write_dot(G, 'current.dot')
 
 con1, con2, con3 = checker('current.dot')
@@ -134,7 +222,6 @@ con1, con2, con3 = checker('current.dot')
 # edges = list(Gn.edges(data=True))
 # edges.sort(key=lambda x: x[1])
 # print(edges)
-
 
 print(str(con1) + " " + str(con2) + " " + str(con3))
 
